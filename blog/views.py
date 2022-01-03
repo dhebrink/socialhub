8fcf831951f7e52aa23c952d260f7454eb737f5d
Daniel Hebrink <danielhebrink@gmail.com>
2022-01-03 12:06:15 -0700

    Add list view for blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from . import models
+
+
+class PostList(generic.ListView):
+    template_name = "blog/index.html"
+    model = models.Post
+    context_object_name = "blog_posts"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.order_by("-created_at")
