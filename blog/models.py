2845da7091f0cff49c6f7c77624be92651c4e507
Daniel Hebrink <danielhebrink@danielssonalmbp.lan>
2022-01-03 11:07:40 -0700

    Initial commit - django setup and blog models

---
+++ blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+SMALL_CHAR_MAX = 50
+MED_CHAR_MAX = 100
+LARGE_CHAR_MAX = 200
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=MED_CHAR_MAX)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=MED_CHAR_MAX)
+    summary = models.CharField(max_length=LARGE_CHAR_MAX, null=True)
+    content = models.TextField(null=False)
+
+    author = models.ForeignKey(Author, on_delete=models.PROTECT, related_name="posts")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+
+    content = models.TextField(null=False)
+
+    submitted_by = models.CharField(max_length=SMALL_CHAR_MAX, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
